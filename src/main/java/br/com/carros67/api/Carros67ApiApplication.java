aafa3130536b283bc1f4fbdec5b27618cc77bcc2
Paulo Bezerra <bezerra.paulo@gmail.com>
2024-10-14 09:55:05 -0400

    first commit

---
+++ src/main/java/br/com/carros67/api/Carros67ApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carros67.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Carros67ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Carros67ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carros67/api/Carros67ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carros67.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Carros67ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
